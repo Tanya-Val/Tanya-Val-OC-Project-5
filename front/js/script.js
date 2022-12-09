d5c69d82de0af58f360c3a3447e0377c61cc9560
Tanya-Val <valachitv@gmail.com>
2022-12-09 16:21:29 +0100

    script.js comments

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,21 @@
-// fetching data on the products from the server
+// Collecting data from the API using the fetch() method. 
+// Collected data contain descriptive information about the products. 
 fetch('http://localhost:3000/api/products/')
         .then(res => res.json())
         .then(data => {
-                console.log(data);
+                //Uncomment next line to print the data from API
+                //console.log(data);
+
+                //The function displays product details from API on the home page using DOM
                 displayProducts(data);
         })
 
 
-//Milestone #3: Inserting the products into the homepage
+//-----Milestone #3: Inserting the products into the homepage-----
+//Inserting all of the products from the API into the website using DOM.
+
+//Function is called in the fetch() method with data from API as an argument.
+//Data is passed through the "for" loop to display product details on the home page using DOM
 function displayProducts(itemList) { 
         let productDisplay = document.querySelector("#items")
         
@@ -21,5 +29,5 @@ function displayProducts(itemList) {
                 </article>
                 </a>
                 `
-        }
-}
+        };
+};
